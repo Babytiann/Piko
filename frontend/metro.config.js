0ee3b1b07e032951a4659d04319fa57cad8863dd
袁峻琦 <yuanjunqi.927@bytedance.com>
2026-02-11 21:55:48 +0800

    feat: fix a bug of wrong package path

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,6 +1,19 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+const path = require('path');
+
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '..');
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [monorepoRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
+
+config.resolver.disableHierarchicalLookup = true;
+
+module.exports = withNativeWind(config, { input: './global.css' });
